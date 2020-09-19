ec47c9309ea367f66d0b7db22a898f430d6f570c
zeritte <zeritte@gmail.com>
2020-09-19 16:45:16 +0300

    maybe finish looping

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def wait_for_dropdown(select):
 
 def selector(selection_type, selection_value, newly_loaded):
     selection = Select(driver.find_element_by_id(selection_type))
-    selection.select_by_value(selection_value.text)
+    selection.select_by_value(selection_value)
 
     loaded_items = driver.find_element_by_id(newly_loaded)
 
@@ -74,6 +74,16 @@ def get_items(year_option, brand_option, model_option, submodel_option):
     return result
 
 
+def reselection_after_submit_form(brand, model):
+    wait_for_dropdown(driver.find_element_by_id("brands"))
+    Select(driver.find_element_by_id("brands")).select_by_value(brand)
+
+    wait_for_dropdown(driver.find_element_by_id("model"))
+    Select(driver.find_element_by_id("model")).select_by_value(model)
+
+    wait_for_dropdown(driver.find_element_by_id("altModel"))
+
+
 def main():
     driver.get("https://www.inciaku.com/tr/akunu-bul/otomobil-hafif-ticari")
     find_button = driver.find_element_by_xpath(
@@ -82,48 +92,51 @@ def main():
     years = driver.find_element_by_id("years")
 
     wait_for_dropdown(years)
-    year_options = years.find_elements_by_tag_name("option")
+    _year_options = years.find_elements_by_tag_name("option")
+
+    year_options = []
+    for year_option in _year_options:
+        year_options.append(year_option.text)
 
     for year_option in year_options:
-        the_year = year_option.text
-        if the_year == "2018":
-            brand_options = selector("years", year_option, "brands")
-            for brand_option in brand_options:
-                the_brand = brand_option.text
-                if the_brand == "VOLKSWAGEN":
-                    model_options = selector("brands", brand_option, "model")
-                    for model_option in model_options:
-                        the_model = model_option.text
-                        if the_model == "PASSAT (3G2)":
-                            submodel_options = selector(
-                                "model", model_option, "altModel")
-                            for submodel_option in submodel_options:
-                                the_submodel = submodel_option.text
-                                submodel_selector = Select(
-                                    driver.find_element_by_id("altModel"))
-                                submodel_selector.select_by_value(the_submodel)
+        _brand_options = selector("years", year_option, "brands")
 
-                                find_button = driver.find_element_by_xpath(
-                                    '//*[@id="find-battery"]/div[2]/button')
-                                find_button.click()
+        brand_options = []
+        for brand_option in _brand_options:
+            brand_options.append(brand_option.text)
 
-                                get_items(
-                                    the_year, the_brand, the_model, the_submodel)
+        for brand_option in brand_options:
+            _model_options = selector("brands", brand_option, "model")
 
-                                wait_for_dropdown(
-                                    driver.find_element_by_id("brands"))
-                                Select(driver.find_element_by_id("brands")).select_by_value(
-                                    the_brand)
+            model_options = []
+            for model_option in _model_options:
+                model_options.append(model_option.text)
 
-                                wait_for_dropdown(
-                                    driver.find_element_by_id("model"))
-                                Select(driver.find_element_by_id("brands")).select_by_value(
-                                    the_model)
+            for model_option in model_options:
+                _submodel_options = selector(
+                    "model", model_option, "altModel")
 
-                                wait_for_dropdown("altModel")
+                submodel_options = []
+                for submodel_option in _submodel_options:
+                    submodel_options.append(submodel_option.text)
 
-                                print(result)
-                                print("=====")
+                for submodel_option in submodel_options:
+                    submodel_selector = Select(
+                        driver.find_element_by_id("altModel"))
+                    submodel_selector.select_by_value(
+                        submodel_option)
+
+                    find_button = driver.find_element_by_xpath(
+                        '//*[@id="find-battery"]/div[2]/button')
+                    find_button.click()
+
+                    get_items(
+                        year_option, brand_option, model_option, submodel_option)
+
+                    reselection_after_submit_form(
+                        brand_option, model_option)
+
+                    print("=====")
 
 
 main()
