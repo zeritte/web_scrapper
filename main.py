540b40a6541f50bdd78e54e434cb2883f585c925
zeritte <zeritte@gmail.com>
2020-09-21 16:11:22 +0300

    have some progress

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 
+
 import time
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -12,25 +13,13 @@ options.headless = True
 driver = webdriver.Chrome(options=options)
 
 
-wb = Workbook()
+wb = load_workbook(filename='Results_Existing.xlsx')
 ws = wb.active
 
-columns = ["Name", "Recommended", "Type", "Year", "Brand", "Model", "Submodel", "Akü Kodu",
-           "Volt (V)", "Kapasite", "CCA", "En", "Boy", "Yükseklik", "Alt Bağlantı Tipi", "Terminal Tipi", "URL"]
 
-ws.append(columns)
-wb.save("Results.xlsx")
-
-
-result = []
-
-
-def load_to_excel(that_row):
-    arr = []
-    for key, value in that_row.items():
-        arr.append(value)
-    ws.append(arr)
-    wb.save("Results.xlsx")
+def save_to_excel(that_row):
+    ws.append(list(that_row.values()))
+    wb.save("Results_New.xlsx")
 
 
 def wait_for_dropdown(select):
@@ -98,8 +87,7 @@ def get_items(year_option, brand_option, model_option, submodel_option):
                         element_data[raw_tags[i]] = raw_tags[i+1]
 
             element_data["URL"] = driver.current_url
-            load_to_excel(element_data)
-            result.append(element_data)
+            save_to_excel(element_data)
 
     except Exception as e:
         print(str(e))
@@ -116,26 +104,26 @@ def reselection_after_submit_form(brand, model):
 
 
 def main():
-    driver.get("https://www.inciaku.com/tr/akunu-bul/otomobil-hafif-ticari")
-    find_button = driver.find_element_by_xpath(
-        '//*[@id="find-battery"]/div[2]/button')
+    year_options = ['1947', '1948', '1949', '1950', '1951', '1952', '1953', '1954', '1955', '1956', '1957', '1958', '1959', '1960', '1961', '1962', '1963', '1964', '1965', '1966', '1967', '1968', '1969', '1970', '1971', '1972', '1973', '1974', '1975', '1976', '1977', '1978', '1979', '1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019']
 
-    years = driver.find_element_by_id("years")
-
-    wait_for_dropdown(years)
-    _year_options = years.find_elements_by_tag_name("option")
-
-    year_options = []
-    for year_option in _year_options[1:]:
-        year_options.append(year_option.text)
-
-    for year_option in year_options:
+    # year_options.reverse()
+    for year_option in year_options[14:]:
         print("YIL", year_option)
+
+        driver.get("https://www.inciaku.com/tr/akunu-bul/otomobil-hafif-ticari")
+        wait_for_dropdown(driver.find_element_by_id("years"))
+
         _brand_options = selector("years", year_option, "brands")
 
         brand_options = []
         for brand_option in _brand_options:
             brand_options.append(brand_option.text)
+
+        # remove
+        print(">>>>>>>>>>")
+        print(brand_options)
+        print("<<<<<<<<<<")
+        # remove
 
         for brand_option in brand_options:
             print("MARKA", brand_option)
